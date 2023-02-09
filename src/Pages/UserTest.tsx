71a2509391e386620cd61ad49d2d5f97a196c8af
Ty <everton_915@yahoo.com>
2023-02-08 18:13:29 -0700

    logging users from backend .get

--- src/Pages/UserTest.tsx
+++ src/Pages/UserTest.tsx
@@ -2,20 +2,28 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function UserTest() {
-  const [users, setUsers] = useState<any>([])
-
   useEffect(() => {
     axios.get('/usersBackEnd').then((response: any) => {
-      setUsers(response.data)
-      console.log('data', users)
+      setUsers(response)
     })
   }, [])
 
+  const [users, setUsers] = useState<any>()
+  const [firstName, setFirstName] = useState<string>('')
+
+  const getUsers = () => {
+    console.log('data', users.data[0].name)
+  }
+
   return (
     <div>
-      {users.map((user: any) => (
-        <div>{user.username}</div>
-      ))}
+      <button
+        onClick={() => {
+          getUsers()
+        }}
+      >
+        Log Users
+      </button>
     </div>
   )
 }
